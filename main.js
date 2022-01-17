1a0705f2864250d081e62e896c93f01a2bc18b12
erickaugustoo <erickkuiz@gmail.com>
2022-01-17 13:02:23 -0300

    config assets

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
+const pessoa = {
+
+  name: 'mayk',
+  age: 37,
+  falar: function(){
+    alert(pessoa.name)
+  }
+}
+
+pessoa.falar()
+
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
