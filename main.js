fc4b5888494d54f320adeac64bd6f40f40f6f3a0
erickaugustoo <erickkuiz@gmail.com>
2022-01-17 15:27:39 -0300

    update site

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-const pessoa = {
-
-  name: 'mayk',
-  age: 37,
-  falar: function(){
-    alert(pessoa.name)
-  }
-}
-
-pessoa.falar()
-
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
@@ -25,3 +14,27 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function(){
+  if(window.scrollY >= navHeight){
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+/* Testimonials carousel slider swiper */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel:true,
+  keyboard: true
+
+});
